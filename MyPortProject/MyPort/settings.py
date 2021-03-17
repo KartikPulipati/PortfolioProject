5b189da350d30bed47fd948e449fc267fc48f1ef
TheBlueCoder09 <59946782+KartikPulipati@users.noreply.github.com>
2021-03-17 11:34:12 -0700

    Update settings.py
    Hid Django_KEY, and changed Debug to False

--- MyPortProject/MyPort/settings.py
+++ MyPortProject/MyPort/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'v$!%d$x-k6-p09pq-5b^rxg9@u_hv=n$7pt1n)%c8s*xhl51jf'
+SECRET_KEY = '${{ secrets.DJANGO_KEY }}'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
